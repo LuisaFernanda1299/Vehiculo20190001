c20f32c11f687b29eb3b097efef548444222554f
Luisa Fernanda <paulinonicasioluisafernanda@gmail.com>
2022-10-31 21:23:33 -0400

    first commit

---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,13 @@
+namespace Vehiculo20190001.Data.Models;
+
+public class Vehiculo
+{
+    public int vehiculoID { get; set; }
+    public string Marca { get; set; }
+
+    public string Modelo { get; set; }
+
+    public int Año { get; set; }
+
+    public string Color { get; set; }
+}
